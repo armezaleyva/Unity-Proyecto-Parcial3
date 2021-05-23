d0b3c76db57f25a36689410d268cb9d9aaa40809
MOSS17 <42181277+MOSS17@users.noreply.github.com>
2021-05-23 04:08:40 -0700

    Network Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MLAPI;
+using MLAPI.NetworkVariable;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
     [SerializeField]
     float moveSpeed = 5f;
@@ -12,6 +14,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     LayerMask whatStopsMovement;
 
+    public override void NetworkStart()
+    {
+        base.NetworkStart();
+        /*foreach(MLAPI.Connection.NetworkClient client in NetworkManager.Singleton.ConnectedClientsList)
+        {
+            Debug.Log(client.PlayerObject.name);
+        }*/
+        /*NetworkManager.Singleton.OnClientConnectedCallback += client =>{
+            Debug.Log(client);
+        };*/
+        //NetworkObject.name = Gamemanager.instance.currentUsername;
+        //Debug.Log(NetworkObject.name);
+        //NetworkManager.Singleton.LocalClientId;
+    }
+
     void Start()
     {
         movePoint.parent = null;
@@ -19,24 +36,26 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        if(IsLocalPlayer){
+            transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f) 
-        {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f) 
             {
-                Vector3 desiredMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-                if (!Physics2D.OverlapCircle(movePoint.position + desiredMovement, .2f, whatStopsMovement)) 
+                if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
                 {
-                    movePoint.position += desiredMovement;
-                }
-            } 
-            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
-            {
-                Vector3 desiredMovement = new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-                if (!Physics2D.OverlapCircle(movePoint.position + desiredMovement, .2f, whatStopsMovement)) 
+                    Vector3 desiredMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                    if (!Physics2D.OverlapCircle(movePoint.position + desiredMovement, .2f, whatStopsMovement)) 
+                    {
+                        movePoint.position += desiredMovement;
+                    }
+                } 
+                else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
                 {
-                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                    Vector3 desiredMovement = new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                    if (!Physics2D.OverlapCircle(movePoint.position + desiredMovement, .2f, whatStopsMovement)) 
+                    {
+                        movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                    }
                 }
             }
         }
