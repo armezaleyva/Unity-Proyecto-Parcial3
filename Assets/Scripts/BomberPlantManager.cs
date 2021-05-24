3d58e21f30e2a625d68d748dba658175a62c4d89
Armando Meza Leyva <armezaleyva@gmail.com>
2021-05-24 12:45:09 -0700

    Remove unused debug buttons

--- Assets/Scripts/BomberPlantManager.cs
+++ Assets/Scripts/BomberPlantManager.cs
@@ -9,7 +9,6 @@ public class BomberPlantManager : MonoBehaviour
         GUILayout.BeginArea(new Rect(10, 10, 300, 300));
         if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
         {
-            StartButtons();
         }
         else
         {
@@ -17,13 +16,6 @@ public class BomberPlantManager : MonoBehaviour
         }
 
         GUILayout.EndArea();
-    }
-
-    static void StartButtons()
-    {
-        if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
-        if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
-        if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
     }
 
     static void StatusLabels()
